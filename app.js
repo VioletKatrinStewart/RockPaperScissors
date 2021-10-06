eeab68d021d3d853e929569f7823ed9533fe7da0
Violet Stewart <vkazoo2@gmail.com>
2021-10-06 16:04:11 -0700

    got my randomThrow function to work

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-import function didUserWin(userThrow, computerThrow) {
+import { didUserWin, getRandomThrow } from './function.js';
 
-}
-// import functions and grab DOM elements
 const submitButton = document.getElementById('submit');
 const winSpan = document.getElementById('wins');
 const lossSpan = document.getElementById('losses');
 const errorMessage = document.getElementById('error');
 const tieSpan = document.getElementById('ties');
 
-let winSpan = 0
-let lossSpan = 0
-let tieSpan = 0
+let wins = 0;
+let losses = 0;
+let ties = 0;
 
-// initialize global state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+submitButton.addEventListener('click', () => {
+  //console.log('click');
+  const selected = document.querySelector('input[type=radio]:checked');
+  const randomThrow = getRandomThrow();
+  console.log(randomThrow);
+});
--- function.js
+++ function.js
@@ -1,10 +1,14 @@
 export function didUserWin(userThrow, computerThrow) {
 
 
-    if (userThrow === computerThrow) return 'draw';
-    else if (userThrow === 'rock' && computerThrow === 'scissors') return 'win';
-    else if (userThrow === 'paper' && computerThrow === 'rock') return 'win';
-    else if (userThrow === 'scissors' && computerThrow === 'paper') return 'win';
-    else return 'lose';
+    if (userThrow === computerThrow) { return 0; }
+    else if (userThrow === 'rock' && computerThrow === 'scissors') { return 1; }
+    else if (userThrow === 'paper' && computerThrow === 'rock') { return 1; }
+    else if (userThrow === 'scissors' && computerThrow === 'paper') { return 1; }
+    else { return 2; }
 
+}
+
+export function getRandomThrow() {
+    { return [0, 1, 2][Math.floor(Math.random() * 3)]; }
 }
