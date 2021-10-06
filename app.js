7467d3785f0fb55c4ed06de3d537f0b1073936b7
Violet Stewart <vkazoo2@gmail.com>
2021-10-06 15:24:20 -0700

    added code for if draw

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+import function didUserWin(userThrow, computerThrow) {
+
+}
 // import functions and grab DOM elements
 const submitButton = document.getElementById('submit');
 const winSpan = document.getElementById('wins');
 const lossSpan = document.getElementById('losses');
 const errorMessage = document.getElementById('error');
+const tieSpan = document.getElementById('ties');
+
+let winSpan = 0
+let lossSpan = 0
+let tieSpan = 0
+
 // initialize global state
 
 // set event listeners 
