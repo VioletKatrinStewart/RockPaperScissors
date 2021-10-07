0adeb5e55454d5e751967149e0ffda229d35a9f4
Violet Stewart <vkazoo2@gmail.com>
2021-10-06 17:06:32 -0700

    working

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const winSpan = document.getElementById('wins');
 const lossSpan = document.getElementById('losses');
 const errorMessage = document.getElementById('error');
 const tieSpan = document.getElementById('ties');
+const resultText = document.getElementById('result');
 
 let wins = 0;
 let losses = 0;
@@ -13,6 +14,32 @@ let ties = 0;
 submitButton.addEventListener('click', () => {
   //console.log('click');
   const selected = document.querySelector('input[type=radio]:checked');
-  const randomThrow = getRandomThrow();
-  console.log(randomThrow);
+  //const randomThrow = getRandomThrow();
+  if (!selected) {
+    return error.classList.remove('hidden');
+  }
+  error.classList.add('hidden');
+
+  const userChoice = selected.value;
+  const computerChoice = getRandomThrow();
+  //console.log(randomThrow);
+
+  if (didUserWin(userChoice, computerChoice) === 1) {
+    wins++;
+    resultText.textContent = "You win!";
+    winSpan.textContent = wins;
+  } else if (didUserWin(userChoice, computerChoice) === 2) {
+    losses++;
+    resultText.textContent = "You lose!";
+    lossSpan.textContent = losses;
+  }
+  else {
+    ties++;
+    tieSpan.textContent = ties;
+  }
+  console.log(computerChoice);
+
+
+
+
 });
--- function.js
+++ function.js
@@ -1,14 +1,21 @@
 export function didUserWin(userThrow, computerThrow) {
 
+    console.log(computerThrow, userThrow);
 
-    if (userThrow === computerThrow) { return 0; }
-    else if (userThrow === 'rock' && computerThrow === 'scissors') { return 1; }
-    else if (userThrow === 'paper' && computerThrow === 'rock') { return 1; }
-    else if (userThrow === 'scissors' && computerThrow === 'paper') { return 1; }
-    else { return 2; }
 
+    if (userThrow === computerThrow) {
+        return 0;
+    } else if (userThrow === 'rock' && computerThrow === 'scissors') {
+        return 1;
+    } else if (userThrow === 'paper' && computerThrow === 'rock') {
+        return 1;
+    } else if (userThrow === 'scissors' && computerThrow === 'paper') {
+        return 1;
+    } else {
+        return 2;
+    }
 }
 
 export function getRandomThrow() {
-    { return [0, 1, 2][Math.floor(Math.random() * 3)]; }
+    return ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
 }
